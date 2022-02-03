7a76ae1fc5e3a88970ad56810da9a70bb479659c
xFerchoVJ <fer_elsword@hotmail.com>
2022-02-03 01:02:29 -0600

    Create the API for books and saving records

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+  Book.create({title: "Crepusculo", description: "Libro de vampiros, donde la morra esta bn pendeja"})
+  Book.create({title: "50 sombras de soli", description: "Libro donde en soli todos kojen y tienen sida"})
+
+  p Book.all
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
